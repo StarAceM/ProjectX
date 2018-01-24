73ef81c61478fde344da12504b796b3c9e351863
StarAceM <star.ace.m@gmail.com>
2018-01-24 10:51:36 -0800

    added relevant dependencies to gradle

---
+++ app/src/main/java/starace/com/projectx/MovieActivity.java
@@ -0,0 +1,16 @@
+package starace.com.projectx;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mstarace on 1/24/18.
+ */
+
+public class MovieActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
