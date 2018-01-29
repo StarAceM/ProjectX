478c8a235278041702738d0abeb6636d3b6f9acb
StarAceM <star.ace.m@gmail.com>
2018-01-29 12:59:01 -0800

    implemented retrofit for splash activity

---
+++ app/src/main/java/starace/com/projectx/ConnectionHelper.java
@@ -0,0 +1,26 @@
+package starace.com.projectx;
+
+/**
+ * Created by mstarace on 1/29/18.
+ */
+
+public class ConnectionHelper {
+
+//    public static boolean checkForConnection() {
+//        ConnectivityManager ConnectionManager = (ConnectivityManager)  getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo networkInfo=ConnectionManager.getActiveNetworkInfo();
+//        if(networkInfo != null && networkInfo.isConnected()==true )
+//            {
+//
+//
+//            }
+//        else
+//            {
+//
+//
+//            }
+//
+//            return false;
+//    }
+
+}
--- app/src/main/java/starace/com/projectx/activities/SplashActivity.java
+++ app/src/main/java/starace/com/projectx/activities/SplashActivity.java
@@ -3,38 +3,31 @@ package starace.com.projectx.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import starace.com.projectx.R;
 import starace.com.projectx.models.GhibliCharacter;
-import starace.com.projectx.retrofit.GhibliClient;
-import starace.com.projectx.retrofit.RetrofitClient;
+import starace.com.projectx.retrofit.RetrofitHelper;
 
-public class SplashActivity extends AppCompatActivity {
+import static starace.com.projectx.retrofit.RetrofitHelper.getRandomCharacter;
+
+
+public class SplashActivity extends AppCompatActivity implements RetrofitHelper.RetrofitCallBack{
+
+    private GhibliCharacter randomCharacter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getRandomCharacter();
+        getRandomCharacter(this);
     }
 
-    private static void getRandomCharacter(){
-        GhibliClient ghibliClient = RetrofitClient.getClient().create(GhibliClient.class);
-        Call<List<GhibliCharacter>> call = ghibliClient.getCharacters();
-        call.enqueue(new Callback<List<GhibliCharacter>>() {
-            @Override
-            public void onResponse(Call<List<GhibliCharacter>> call, Response<List<GhibliCharacter>> response) {
+    @Override
+    public void randomCharacterCallBack(GhibliCharacter character) {
+        randomCharacter = character;
+        initViews();
+    }
 
-            }
+    private void initViews() {
 
-            @Override
-            public void onFailure(Call<List<GhibliCharacter>> call, Throwable t) {
-
-            }
-        });
     }
 }
