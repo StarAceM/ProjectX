07ca4934ebf912268ca5ec16ba037e7fd9c3fb63
StarAceM <star.ace.m@gmail.com>
2018-01-29 13:40:15 -0800

    added splash random charcter text and connection check

--- app/src/main/java/starace/com/projectx/ConnectionHelper.java
+++ app/src/main/java/starace/com/projectx/ConnectionHelper.java
@@ -1,26 +1,24 @@
 package starace.com.projectx;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by mstarace on 1/29/18.
  */
 
 public class ConnectionHelper {
 
-//    public static boolean checkForConnection() {
-//        ConnectivityManager ConnectionManager = (ConnectivityManager)  getSystemService(Context.CONNECTIVITY_SERVICE);
-//        NetworkInfo networkInfo=ConnectionManager.getActiveNetworkInfo();
-//        if(networkInfo != null && networkInfo.isConnected()==true )
-//            {
-//
-//
-//            }
-//        else
-//            {
-//
-//
-//            }
-//
-//            return false;
-//    }
+    public static boolean checkForConnection(Context context) {
+        ConnectivityManager ConnectionManager = (ConnectivityManager)  context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(ConnectionManager != null) {
+            NetworkInfo networkInfo = ConnectionManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                return true;
+            }
+        }
 
+        return false;
+    }
 }
--- app/src/main/java/starace/com/projectx/activities/SplashActivity.java
+++ app/src/main/java/starace/com/projectx/activities/SplashActivity.java
@@ -2,7 +2,11 @@ package starace.com.projectx.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import starace.com.projectx.ConnectionHelper;
 import starace.com.projectx.R;
 import starace.com.projectx.models.GhibliCharacter;
 import starace.com.projectx.retrofit.RetrofitHelper;
@@ -18,7 +22,11 @@ public class SplashActivity extends AppCompatActivity implements RetrofitHelper.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getRandomCharacter(this);
+        if(ConnectionHelper.checkForConnection(getApplicationContext())) {
+            getRandomCharacter(this);
+        } else {
+            //todo handle non-connected device
+        }
     }
 
     @Override
@@ -28,6 +36,21 @@ public class SplashActivity extends AppCompatActivity implements RetrofitHelper.
     }
 
     private void initViews() {
+        if(randomCharacter != null) {
+            if(!randomCharacter.getName().isEmpty()) {
+                TextView nameText = findViewById(R.id.splash_name_text);
+                nameText.setText(randomCharacter.getName());
+            } else {
+                //todo handle empty name
+            }
+        }
 
+        Button enterButton = findViewById(R.id.splash_enter_button);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //todo go to film activity
+            }
+        });
     }
 }
